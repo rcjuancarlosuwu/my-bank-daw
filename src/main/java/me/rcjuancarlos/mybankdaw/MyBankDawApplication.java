42beab9038a6401fe716bdbea08d347c7d9dd486
Juan Carlos Ramón Condezo <rcjuancarlos01@gmail.com>
2022-08-18 23:22:28 -0500

    feat: add my bank daw application

---
+++ src/main/java/me/rcjuancarlos/mybankdaw/MyBankDawApplication.java
@@ -0,0 +1,13 @@
+package me.rcjuancarlos.mybankdaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBankDawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBankDawApplication.class, args);
+    }
+
+}
