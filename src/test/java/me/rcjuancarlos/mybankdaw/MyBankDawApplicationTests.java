8c34b24c6c12c2ccce88579bc681c9f0d13d3aac
Juan Carlos Ramón Condezo <rcjuancarlos01@gmail.com>
2022-08-18 23:21:14 -0500

    chore: project setup

---
+++ src/test/java/me/rcjuancarlos/mybankdaw/MyBankDawApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rcjuancarlos.mybankdaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBankDawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
